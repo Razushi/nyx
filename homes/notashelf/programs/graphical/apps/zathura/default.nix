ab0dd19053eceb8d460faa444102802ca1c66268
NotAShelf <raf@notashelf.dev>
2024-06-29 10:14:09 +0300

    apps/zathura: refactor configuration

--- homes/notashelf/programs/graphical/apps/zathura/default.nix
+++ homes/notashelf/programs/graphical/apps/zathura/default.nix
@@ -4,34 +4,60 @@
   lib,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib.modules) mkIf;
   inherit (osConfig) modules;
 
   sys = modules.system;
   prg = sys.programs;
 in {
   config = mkIf prg.zathura.enable {
-    xdg.configFile."zathura/catppuccin-mocha".source = pkgs.fetchurl {
+    xdg.configFile."zathura/catppuccin-mocha-theme".source = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/catppuccin/zathura/main/src/catppuccin-mocha";
-      hash = "sha256-/HXecio3My2eXTpY7JoYiN9mnXsps4PAThDPs4OCsAk=";
+      hash = "sha256-POxMpm77Pd0qywy/jYzZBXF/uAKHSQ0hwtXD4wl8S2Q=";
     };
 
     programs.zathura = {
       enable = true;
-      extraConfig = "include catppuccin-mocha";
-
+      # <https://man.archlinux.org/man/zathurarc.5>
       options = {
+        database = "sqlite"; # or plain, which is the default but also deprecated
+        sandbox = "normal";
         font = "Iosevka 15";
         selection-clipboard = "clipboard";
         adjust-open = "best-fit";
-        pages-per-row = "1";
-        scroll-page-aware = "true";
+        n-completion-items = 20;
+        continuous-hist-save = false;
+
+        # Zoom
+        link-zoom = true;
+        zoom-min = 10;
+
+        # Page options
+        pages-per-row = 1;
+        page-cache-size = 25;
+        page-padding = 3; # gap inbetween pages
+
+        # Statusbar
+        guioptions = "s"; # enable statusbar
+        statusbar-basename = true;
+        statusbar-home-tilde = true;
+        statusbar-page-percent = true;
+        statusbar-v-padding = 2;
+        statusbar-h-padding = 6;
+
+        # Scroll
+        scroll-page-aware = true;
         scroll-full-overlap = "0.01";
-        scroll-step = "100";
-        smooth-scroll = true;
-        zoom-min = "10";
-        guioptions = "none";
+        scroll-step = 100;
       };
+
+      mappings = {
+        D = "toggle_page_mode";
+      };
+
+      extraConfig = ''
+        include catppuccin-mocha-theme
+      '';
     };
   };
 }
